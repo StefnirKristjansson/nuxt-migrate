d20373980d63c3d004da79a84c0f06772c08a511
Stefnirk <stefnir@proency.com>
2022-05-06 15:56:51 +0000

    ini

---
+++ nuxt.config.js
@@ -0,0 +1,72 @@
+export default {
+  // loading: "~/components/loading.vue",
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  ssr: false,
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "server",
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Responsive Bootstrap 5 Admin Dashboard"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  router: {
+    // linkExactActiveClass: 'active'
+  },
+  /*
+   ** Global CSS
+   */
+  css: ["~/assets/scss/app.scss"],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [
+    { src: '~/plugins/i18n.js', mode: 'client' },
+    { src: '~/plugins/vue-apexcharts.js', mode: 'client' },
+
+  ],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    "bootstrap-vue/nuxt",
+    // Doc: https://github.com/nuxt/content
+    "@nuxt/content"
+  ],
+  /*
+   ** Content module configuration
+   ** See https://content.nuxtjs.org/configuration
+   */
+  content: {},
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+};
